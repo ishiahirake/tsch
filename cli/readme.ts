c1b9817db5ebb4be5a4368e0cc285994e0d03f2a
ishiahirake <44836326+ishiahirake@users.noreply.github.com>
2022-06-08 08:40:43 +0900

    Implement statistic. (#26)

--- cli/readme.ts
+++ cli/readme.ts
@@ -1,10 +1,17 @@
 // Project README.md file
+import { difficultyLevels } from "./question"
+import { Question } from "./question"
 
 import { EOL } from "os"
 import { read, write } from "./fs"
-import { getWorkingDirPath } from "./path"
-import { Statistic } from "./statistic"
+import {
+  getQuestionReadmeRelativePath,
+  getQuestionSolutionRelativePath,
+  getWorkingDirPath,
+} from "./path"
 import { getQuestions } from "./store"
+import { pad, PadDirection, ucfirst } from "./utils"
+import { analyze } from "./statistic"
 
 const README_FILE = "README.md"
 const TPL_FILE = "README.tpl.md"
@@ -13,10 +20,10 @@ const REPLACE_REGEX = /^:(.*?):$/
 
 export async function updateReadme() {
   const questions = await getQuestions()
-  const statistic = new Statistic(questions)
 
   const replacements: Record<string, string> = {
-    "done-questions": statistic.getDoneTable(),
+    statistic: getStatisticInfo(questions),
+    "done-questions": getDoneTable(questions),
   }
 
   const tpl = read(getWorkingDirPath(TPL_FILE))
@@ -29,4 +36,101 @@ export async function updateReadme() {
     .join(EOL)
 
   write(getWorkingDirPath(README_FILE), filledTpl)
+}
+
+//
+
+function getStatisticInfo(questions: Question[]): string {
+  const statistic = analyze(questions)
+  const total =
+    getProgressInfo("Total", statistic.doneQuestionCount, statistic.totalQuestionCount) + "  "
+  const difficulties = Object.keys(difficultyLevels).sort((l1, l2) => {
+    return difficultyLevels[l1] - difficultyLevels[l2]
+  })
+
+  const detail = difficulties
+    .map((difficulty) => {
+      return getProgressInfo(
+        difficulty,
+        statistic.doneDifficultyCountMap[difficulty],
+        statistic.totalDifficultyCountMap[difficulty]
+      )
+    })
+    .join(" | ")
+
+  return [total, detail].join("\n")
+}
+
+function getProgressInfo(title: string, current: number, total: number): string {
+  const percentage = ((current / total) * 100).toFixed(0) + "%"
+  return `${ucfirst(title)}: ${percentage} (${current}/${total})`
+}
+
+//
+
+type Column = "no" | "difficulty" | "challenge" | "solution"
+type RowData<T = string> = Record<Column, T>
+
+const COLUMN_ORDERS: Column[] = ["no", "difficulty", "challenge", "solution"]
+const COLUMN_PAD_DIRECTIONS: RowData<PadDirection> = {
+  no: "left",
+  difficulty: "right",
+  challenge: "right",
+  solution: "right",
+}
+
+function getDoneTable(questions: Question[]): string {
+  questions = questions.filter((q) => q.done).sort((a, b) => a.no - b.no)
+
+  const rows = questions.map<RowData>((q) => {
+    return {
+      no: `${q.no}`,
+      difficulty: q.difficulty,
+      challenge: `[${q.name}](${getQuestionReadmeRelativePath(q.fullName)})`,
+      solution: `[Solution](${getQuestionSolutionRelativePath(q.fullName)})`,
+    }
+  })
+
+  const title: RowData = {
+    no: "NO",
+    difficulty: "Difficulty",
+    challenge: "Challenge",
+    solution: "Solution",
+  }
+  const division: RowData = {
+    no: "---:",
+    difficulty: "-",
+    challenge: "-",
+    solution: "-",
+  }
+
+  // column length no include the front/end 2 space
+  const columnMaxLength: RowData<number> = { no: 0, difficulty: 0, challenge: 0, solution: 0 }
+
+  const keys = Object.keys(title) as Column[]
+  for (const row of rows.concat(title, division)) {
+    for (const key of keys) {
+      const keyLength = row[key].length
+      if (keyLength > columnMaxLength[key]) {
+        columnMaxLength[key] = keyLength
+      }
+    }
+  }
+
+  const table: string[] = [
+    getRowString(title, columnMaxLength),
+    getRowString(division, columnMaxLength, "-"),
+  ]
+
+  return table.concat(rows.map((row) => getRowString(row, columnMaxLength))).join("\n")
+}
+
+function getRowString(row: RowData, columnLength: RowData<number>, padStr = " "): string {
+  const sep = "|"
+
+  const content = COLUMN_ORDERS.map(
+    (col) => ` ${pad(row[col], columnLength[col], COLUMN_PAD_DIRECTIONS[col], padStr)} `
+  ).join(sep)
+
+  return sep + content + sep
 }
--- cli/statistic.ts
+++ cli/statistic.ts
@@ -1,117 +1,53 @@
-import { getQuestionReadmeRelativePath, getQuestionSolutionRelativePath } from "./path"
-import { Question } from "./question"
+import { difficultyLevels, Question } from "./question"
 
 type DifficultyCountMap = Record<string, number>
 
-type Column = "no" | "difficulty" | "challenge" | "solution"
-type RowData<T = string> = Record<Column, T>
+type Statistic = {
+  totalQuestionCount: number
+  totalDifficultyCountMap: DifficultyCountMap
 
-type PadDirection = "left" | "right"
-
-const COLUMN_ORDERS: Column[] = ["no", "difficulty", "challenge", "solution"]
-const COLUMN_PAD_DIRECTIONS: RowData<PadDirection> = {
-  no: "left",
-  difficulty: "right",
-  challenge: "right",
-  solution: "right",
+  doneQuestionCount: number
+  doneDifficultyCountMap: DifficultyCountMap
 }
 
-export class Statistic {
-  private totalQuestionCount = 0
-  private totalDifficultyCountMap: DifficultyCountMap = {}
+export function analyze(questions: Question[]): Readonly<Statistic> {
+  const totalQuestionCount = questions.length
 
-  private doneQuestionCount = 0
-  private doneDifficultyCountMap: DifficultyCountMap = {}
+  const totalDifficultyCountMap = makeDifficultyCountMap()
 
-  constructor(private readonly questions: Question[]) {}
+  let doneQuestionCount = 0
+  const doneDifficultyCountMap = makeDifficultyCountMap()
 
-  analyze() {
-    this.totalQuestionCount = this.questions.length
+  for (const question of questions) {
+    const difficulty = question.difficulty
 
-    for (const question of this.questions) {
-      const difficulty = question.difficulty
+    increaseDifficultyCountMap(totalDifficultyCountMap, difficulty)
 
-      this.increaseDifficultyCountMap(this.totalDifficultyCountMap, difficulty)
-
-      if (question.done) {
-        this.doneQuestionCount += 1
-        this.increaseDifficultyCountMap(this.doneDifficultyCountMap, difficulty)
-      }
+    if (question.done) {
+      doneQuestionCount += 1
+      increaseDifficultyCountMap(doneDifficultyCountMap, difficulty)
     }
   }
 
-  private increaseDifficultyCountMap(map: DifficultyCountMap, difficulty: string) {
-    if (map[difficulty] === undefined) {
-      map[difficulty] = 1
-    } else {
-      map[difficulty] += 1
-    }
+  return {
+    totalQuestionCount,
+    totalDifficultyCountMap,
+    doneQuestionCount,
+    doneDifficultyCountMap,
   }
+}
 
-  getDoneTable(): string {
-    const questions = this.questions.filter((q) => q.done).sort((a, b) => a.no - b.no)
+function makeDifficultyCountMap(): DifficultyCountMap {
+  return Object.keys(difficultyLevels).reduce((map, difficulty) => {
+    map[difficulty] = 0
+    return map
+  }, {} as DifficultyCountMap)
+}
 
-    const rows = questions.map<RowData>((q) => {
-      return {
-        no: `${q.no}`,
-        difficulty: q.difficulty,
-        challenge: `[${q.name}](${getQuestionReadmeRelativePath(q.fullName)})`,
-        solution: `[Solution](${getQuestionSolutionRelativePath(q.fullName)})`,
-      }
-    })
-
-    const title: RowData = {
-      no: "NO",
-      difficulty: "Difficulty",
-      challenge: "Challenge",
-      solution: "Solution",
-    }
-    const division: RowData = {
-      no: "---:",
-      difficulty: "-",
-      challenge: "-",
-      solution: "-",
-    }
-
-    // column length no include the front/end 2 space
-    const columnMaxLength: RowData<number> = { no: 0, difficulty: 0, challenge: 0, solution: 0 }
-
-    const keys = Object.keys(title) as Column[]
-    for (const row of rows.concat(title, division)) {
-      for (const key of keys) {
-        const keyLength = row[key].length
-        if (keyLength > columnMaxLength[key]) {
-          columnMaxLength[key] = keyLength
-        }
-      }
-    }
-
-    const table: string[] = [
-      this.getRowString(title, columnMaxLength),
-      this.getRowString(division, columnMaxLength, "-"),
-    ]
-
-    return table.concat(rows.map((row) => this.getRowString(row, columnMaxLength))).join("\n")
-  }
-
-  private getRowString(row: RowData, columnLength: RowData<number>, pad = " "): string {
-    const sep = "|"
-
-    const content = COLUMN_ORDERS.map(
-      (col) => ` ${this.pad(row[col], columnLength[col], COLUMN_PAD_DIRECTIONS[col], pad)} `
-    ).join(sep)
-
-    return sep + content + sep
-  }
-
-  private pad(str: string, length: number, dir: PadDirection, pad = " ") {
-    const count = length - str.length
-    if (count <= 0) {
-      return str
-    }
-
-    const padStr = pad.repeat(count)
-
-    return dir === "left" ? padStr + str : str + padStr
+function increaseDifficultyCountMap(map: DifficultyCountMap, difficulty: string) {
+  if (map[difficulty] === undefined) {
+    map[difficulty] = 1
+  } else {
+    map[difficulty] += 1
   }
 }
