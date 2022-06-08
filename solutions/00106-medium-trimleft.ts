6feea2bb7e67623034b6d00ed8f1b41524083351
ishiahirake <44836326+ishiahirake@users.noreply.github.com>
2022-06-08 21:45:28 +0900

    Add solution for 04803-medium-trim-right (#31)

--- solutions/00106-medium-trimleft.ts
+++ solutions/00106-medium-trimleft.ts
@@ -3,3 +3,7 @@ type TrimLeft<S extends string> = S extends `${infer C}${infer REST}`
     ? TrimLeft<REST>
     : S
   : S
+
+// The following is more compact
+//
+// type TrimLeft<S extends string> = S extends `${" " | "\n" | "\t"}${infer REST}` ? TrimLeft<REST> : S
