e6aba5d1582b1ccf38a79bc1476e136cba7fd834
ishiahirake <ishiahirake@gmail.com>
2022-06-14 08:48:10 +0900

    Add solution for 05360-medium-unique.

--- solutions/05153-medium-indexof.ts
+++ solutions/05153-medium-indexof.ts
@@ -1,7 +1,9 @@
-type IndexOfMatch<I, U> = U extends I ? true : false
+type IndexOfMatch<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2
+  ? true
+  : false
 
 type IndexOf<T extends unknown[], U, P extends unknown[] = []> = T extends [infer I, ...infer REST]
-  ? IndexOfMatch<I, U> extends true
+  ? MyIncludesEqual<I, U> extends true
     ? P["length"]
     : IndexOf<REST, U, [...P, ""]>
   : -1
