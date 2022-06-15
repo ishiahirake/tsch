cc0e492c77de6421484b94bd48678ecadd8b33b8
ishiahirake <ishiahirake@gmail.com>
2022-06-15 21:41:41 +0900

    Add another solution for 00006-hard-simple-vue.

--- solutions/00006-hard-simple-vue.ts
+++ solutions/00006-hard-simple-vue.ts
@@ -14,3 +14,22 @@ declare function SimpleVue<
   computed: Computed
   methods: SimpleVueMethods<Data & SimpleVueComputedObject<Computed>>
 }): any
+
+/**
+ * With ThisType utility type, we can also implements it by following code.
+ *
+ * @see {@link https://www.typescriptlang.org/docs/handbook/utility-types.html#thistypetype}
+ *
+ * @param options
+ */
+// declare function SimpleVue<D, C, M>(options: {
+//   data(this: unknown): D
+//   computed: C & ThisType<D>
+//   methods: M &
+//     ThisType<
+//       D &
+//         M & {
+//           [P in keyof C]: C[P] extends () => infer R ? R : C[P]
+//         }
+//     >
+// }): D & C & M
